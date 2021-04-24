997db292a698cf9f8047a19cdefc0b467ad5dc8b
bartoszkordek <bartosz.kordek10@gmail.com>
2021-04-24 23:39:36 +0200

    italic

--- main/main.py
+++ main/main.py
@@ -3,7 +3,7 @@
 # -----------------------------------------------------------------------------
 
 tokens = (
-    'TEXT','BEGIN', 'END', 'DOCUMENTCLASS', 'BOLD'
+    'TEXT','BEGIN', 'END', 'DOCUMENTCLASS', 'BOLD', 'ITALIC'
     )
 
 # Tokens
@@ -12,6 +12,7 @@ t_BEGIN         = r'\\begin\{[a-zA-Z0-9_ ]*\}'
 t_END           = r'\\end\{[a-zA-Z0-9_ ]*\}'
 t_DOCUMENTCLASS = r'\\documentclass\[.*\]\{[a-zA-Z0-9_ ]*\}'
 t_BOLD          = r'\\textbf\{[a-zA-Z0-9_ ]*\}'
+t_ITALIC        = r'\\textit\{[a-zA-Z0-9_ ]*\}'
 t_TEXT          = r'[a-zA-Z0-9_! ]*[a-zA-Z0-9_!]'
 
 # Ignored characters
@@ -34,21 +35,22 @@ def p_statement_expr(p):
     'statement : expression'
     print(p[1])
 
-def p_expression_begin(p):
-    'expression : BEGIN expression'
+def p_expression_begin_end(p):
+    'expression : BEGIN expression END'
     try:
-        p[0] = '<html>' + '<head>' + '</head>' + '<body>' + p[2]
+        p[0] = '<html>' + '<head>' + '</head>' + '<body>' + p[2] + '</body>' + '</html>'
     except LookupError:
         print(f"Undefined name {p[1]!r}")
         p[0] = 0
-		
-def p_expression_end(p):
-    'expression : expression END'
+
+def p_expression_sample(p):
+    'expression : TEXT expression BOLD BOLD expression'
     try:
-        p[0] = p[1] + '</body>' + '</html>'
+        p[0] = 'TEST: ' + p[1] + p[2] + p[3] + p[4] + p[5]
     except LookupError:
-        print(f"Undefined name {p[1]!r}")
+        print(f"Undefined name {p[1]!r} or {p[2]!r} ")
         p[0] = 0
+
 
 def p_expression_bold(p):
     'expression : BOLD'
@@ -61,6 +63,20 @@ def p_expression_bold(p):
     except LookupError:
         print(f"Undefined name {p[1]!r}")
         p[0] = 0
+
+
+def p_expression_italic(p):
+    'expression : ITALIC'
+    try:
+        input_label = p[1]
+        open_bracket = input_label.index('{')
+        close_bracket = input_label.index('}')
+        extracted_italic_text = input_label[open_bracket+1 : close_bracket]
+        p[0] =  '<i>' + extracted_italic_text + '<i>'
+    except LookupError:
+        print(f"Undefined name {p[1]!r}")
+        p[0] = 0
+
 
 def p_expression_text(p):
     'expression : TEXT'
