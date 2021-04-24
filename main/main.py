e8e774ddc20006454938d125b81e8ca4d1d975a3
bartoszkordek <bartosz.kordek10@gmail.com>
2021-04-25 01:47:00 +0200

    updates multi expressions

--- main/main.py
+++ main/main.py
@@ -3,14 +3,14 @@
 # -----------------------------------------------------------------------------
 
 tokens = (
-    'TEXT','BEGIN', 'END', 'DOCUMENTCLASS', 'BOLD', 'ITALIC'
+    'TEXT','BEGIN', 'END', 'DOCUMENTCLASS', 'BOLD', 'ITALIC', 'NEWLINE'
     )
 
 # Tokens
 
 t_BEGIN         = r'\\begin\{[a-zA-Z0-9_ ]*\}'
 t_END           = r'\\end\{[a-zA-Z0-9_ ]*\}'
-t_DOCUMENTCLASS = r'\\documentclass\[.*\]\{[a-zA-Z0-9_ ]*\}'
+t_DOCUMENTCLASS = r'\\documentclass\[.*px\]\{[a-zA-Z0-9_ ]*\}'
 t_BOLD          = r'\\textbf\{[a-zA-Z0-9_ ]*\}'
 t_ITALIC        = r'\\textit\{[a-zA-Z0-9_ ]*\}'
 t_TEXT          = r'[a-zA-Z0-9_! ]*[a-zA-Z0-9_!]'
@@ -36,50 +36,49 @@ def p_statement_expr(p):
     print(p[1])
 
 def p_expression_begin_end(p):
-    'expression : BEGIN expression END'
+    '''expression : BEGIN multiexpression END'''
     try:
-        p[0] = '<html>' + '<head>' + '</head>' + '<body>' + p[2] + '</body>' + '</html>'
+        p[0] = '<!DOCTYPE html>' +'\n'+ '<html>' +'\n'+ '<head>' +'\n'+ '</head>' +'\n'+ '<body>' +'\n'+ p[2] +'\n'+ '</body>' +'\n'+ '</html>'
     except LookupError:
-        print(f"Undefined name {p[1]!r}")
+        print(f"Undefined name {p[2]!r}")
         p[0] = 0
 
-def p_expression_sample(p):
-    'expression : TEXT expression BOLD BOLD expression'
+def p_expression_multi(p):
+    'multiexpression : multiexpression multiexpression'
     try:
-        p[0] = 'TEST: ' + p[1] + p[2] + p[3] + p[4] + p[5]
+        p[0] = p[1] +'\n'+ p[2]
     except LookupError:
-        print(f"Undefined name {p[1]!r} or {p[2]!r} ")
+        print(f"Undefined name {p[1]!r} or {p[2]!r}")
         p[0] = 0
-
 
 def p_expression_bold(p):
-    'expression : BOLD'
+    'multiexpression : BOLD'
     try:
         input_label = p[1]
         open_bracket = input_label.index('{')
         close_bracket = input_label.index('}')
         extracted_bolded_text = input_label[open_bracket+1 : close_bracket]
-        p[0] =  '<b>' + extracted_bolded_text + '<b>'
+        p[0] =  '<b>' +'\n'+ extracted_bolded_text +'\n'+ '</b>'
     except LookupError:
         print(f"Undefined name {p[1]!r}")
         p[0] = 0
 
 
 def p_expression_italic(p):
-    'expression : ITALIC'
+    'multiexpression : ITALIC'
     try:
         input_label = p[1]
         open_bracket = input_label.index('{')
         close_bracket = input_label.index('}')
         extracted_italic_text = input_label[open_bracket+1 : close_bracket]
-        p[0] =  '<i>' + extracted_italic_text + '<i>'
+        p[0] = '<i>' +'\n'+ extracted_italic_text +'\n'+ '</i>'
     except LookupError:
         print(f"Undefined name {p[1]!r}")
         p[0] = 0
 
 
 def p_expression_text(p):
-    'expression : TEXT'
+    'multiexpression : TEXT'
     try:
         p[0] = p[1]
     except LookupError:
@@ -90,12 +89,13 @@ import ply.yacc as yacc
 yacc.yacc()
 
 #read from file
-#f = open("sample.tex", "r")
-#yacc.parse(f.read())
+f = open("main/input/sample.tex", "r")
+yacc.parse(f.read())
 
-while True:
-    try:
-        s = input('result > ')
-    except EOFError:
-        break
-    yacc.parse(s)
+#read from console
+# while True:
+#     try:
+#         s = input('result > ')
+#     except EOFError:
+#         break
+#     yacc.parse(s)
